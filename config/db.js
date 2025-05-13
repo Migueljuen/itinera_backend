ef7c9da2f584073a6eb44b43c76f7c20ae5ba09b
Miguel Juen <167290997+NanduduraNgPWD@users.noreply.github.com>
2025-05-13 01:26:59 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -11,16 +11,10 @@
 // module.exports = pool.promise();
 
 const mysql = require('mysql2');
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+require('dotenv').config();
+
+// Create a connection pool using the connection string
+const pool = mysql.createPool(process.env.DATABASE_URL);
+
 module.exports = pool.promise();
+
